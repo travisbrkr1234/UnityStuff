9d288a3aa40669c6dc6a79e351c355ee04dd7e6c
travisbrkr1234 <carlos.ochoa@infusionsoft.com>
2017-04-18 23:37:38 -0700

    lecture 2 text game

---
+++ Text101/CountdownTimer.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class CountdownTimer : MonoBehaviour {
+    public Text timerText;
+    float timeLeft = 60;
+
+    // Use this for initialization
+    void Start () {
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        timeLeft -= Time.deltaTime;
+    }
+
+    private void OnGUI() {
+        if (timeLeft > 0) {
+            timerText.text = "Time Remaining: " + string.Format("{0:0}", timeLeft);
+        } else {
+            timerText.text = "Time's up!";
+        }
+    }
+}
