9f79cdbccfdfc6f6ef53753a6b632ecb146fdc6d
travisbrkr1234 <carlos.ochoa@infusionsoft.com>
2017-04-19 20:57:19 -0700

    text101 part 2

--- Text101/TextController.cs
+++ Text101/TextController.cs
@@ -1,11 +1,12 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
-using System.Collections;
 
 public class TextController : MonoBehaviour {
 
     public Text text;
-    private enum States { cell, mirror, cell_mirror, stuff_0, stuff_1, lock_0, lock_1, freedom };
+    private enum States { cell, mirror, cell_mirror, stuff_0, stuff_1, lock_0, lock_1, 
+    corridor_0
+        };
     private States myState;
     float timeLeft = 60.0f;
 
@@ -17,51 +18,59 @@ public class TextController : MonoBehaviour {
     // Update is called once per frame
     void Update() {
         print(myState);
-        if (myState == States.cell) { state_cell(); } else if (myState == States.stuff_0) { state_stuff_0(); } else if (myState == States.stuff_1) { state_stuff_1(); } else if (myState == States.lock_0) { state_lock_0(); } else if (myState == States.lock_1) { state_lock_1(); } else if (myState == States.mirror) { state_mirror(); } else if (myState == States.cell_mirror) { state_cell_mirror(); } else if (myState == States.freedom) { state_freedom(); }
+        if      (myState == States.cell) 		{ cell(); } 
+        else if (myState == States.stuff_0) 	{ stuff_0(); } 
+        else if (myState == States.stuff_1) 	{ stuff_1(); } 
+        else if (myState == States.lock_0) 		{ lock_0(); } 
+        else if (myState == States.lock_1) 		{ lock_1(); } 
+        else if (myState == States.mirror) 		{ mirror(); } 
+        else if (myState == States.cell_mirror) { cell_mirror(); } 
+        else if (myState == States.corridor_0) 	{ corridor_0(); }
     }
-    void state_cell() {
+    void cell() {
         text.text = "Escape the room, time starts now \n\n" +
         "Press the S key to view Stuff \nM to view Mirror \nL to view Lock";
         if (Input.GetKeyDown(KeyCode.S)) { myState = States.stuff_0; } 
         else if (Input.GetKeyDown(KeyCode.M)) { myState = States.mirror; } 
         else if (Input.GetKeyDown(KeyCode.L)) { myState = States.lock_0; }
     }
-    void state_mirror() {
+    void mirror() {
         text.text = "In the mirrors reflection, you see a bobby pin in your shirt pocket \n\n" +
         "Press T to Take the bobby pin or R to Return to your cell";
         if (Input.GetKeyDown(KeyCode.T)) { myState = States.cell_mirror; } 
         else if (Input.GetKeyDown(KeyCode.R)) { myState = States.cell; }
     }
-    void state_stuff_0() {
+    void stuff_0() {
         text.text = "Oh! Whats this? A bag full of tools!?! \n\n" +
         "Press R to Return to your cell";
         if (Input.GetKeyDown(KeyCode.R)) { myState = States.cell; }
     }
-    void state_stuff_1() {
+    void stuff_1() {
         text.text = "You have discovered a hammer and screwdriver in your bag of tools." +
-        "Press R to Return to your cell";
+        "\nPress R to Return to your cell";
         if (Input.GetKeyDown(KeyCode.R)) { myState = States.cell_mirror; }
     }
-    void state_lock_0() {
+    void lock_0() {
         text.text = "This lock requires some kind of combination, check around for clues \n\n" +
         "Press the R key to your cell";
         if (Input.GetKeyDown(KeyCode.R)) { myState = States.cell; }
     }
-    void state_lock_1() {
+    void lock_1() {
         text.text = "Using the bobby pin and some ingenuity, you are able to pick the lock!" +
         "Press O to Open, or R to Return to your cell";
-        if (Input.GetKeyDown(KeyCode.O)) { myState = States.freedom; } 
+        if (Input.GetKeyDown(KeyCode.O)) { myState = States.corridor_0; } 
         else if (Input.GetKeyDown(KeyCode.R)) { myState = States.cell_mirror; }
     }
-    void state_cell_mirror() {
+    void cell_mirror() {
         text.text = "You are still in your cell, and you STILL want to escape! There is a " +
-        "sheet of folded paper on the floor, \nPress S to grab the Sheet" +
-        "\nPress S to view Sheets, or L to view Lock";
+        "sheet of folded paper on the floor \nPress S to grab the Sheet" +
+        "\nL to view Lock";
         if (Input.GetKeyDown(KeyCode.S)) { myState = States.stuff_1; } 
         else if (Input.GetKeyDown(KeyCode.L)) { myState = States.lock_1; }
     }
-    void state_freedom() {
-        text.text = "You are FREE!\n\n" +
+    void corridor_0() {
+        text.text = "You are now in the corridor, with a door on the right and left and one " +
+            "directly ahead. \nHow would you like to proceed \n\n" +
         "Press P to Play again";
         if (Input.GetKeyDown(KeyCode.P)) { myState = States.cell; }
     }
